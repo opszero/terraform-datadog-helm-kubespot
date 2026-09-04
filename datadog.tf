9c3d9f9b5a65db9e001e04d8341b4d7e69be307a
Abhi Yerra <abhi@opszero.com>
2026-09-04 16:18:43 -0700

    Added app key

--- datadog.tf
+++ datadog.tf
@@ -9,11 +9,84 @@ resource "helm_release" "datadog" {
   values = [
     "${file("${path.module}/datadog-values.yaml")}"
   ]
+}
 
-  set = [
-    {
-      name  = "datadog.apiKey"
-      value = var.datadog_api_key
-    },
+resource "kubernetes_manifest" "datadog_agent" {
+  manifest = {
+    apiVersion = "datadoghq.com/v2alpha1"
+    kind       = "DatadogAgent"
+
+    metadata = {
+      name      = "datadog"
+      namespace = "datadog"
+    }
+
+    spec = {
+      features = {
+        apm = {
+          enabled = true
+
+          hostPortConfig = {
+            enabled = true
+          }
+
+          instrumentation = {
+            enabled = true
+          }
+
+          unixDomainSocketConfig = {
+            enabled = true
+          }
+        }
+
+        dogstatsd = {
+          hostPortConfig = {
+            enabled = true
+          }
+        }
+
+        externalMetricsServer = {
+          enabled = true
+        }
+
+        kubeStateMetricsCore = {
+          enabled = true
+        }
+
+        liveContainerCollection = {
+          enabled = true
+        }
+
+        liveProcessCollection = {
+          enabled = true
+        }
+
+        logCollection = {
+          containerCollectAll = true
+          enabled             = true
+        }
+
+        npm = {
+          enabled = true
+        }
+
+        orchestratorExplorer = {
+          enabled = true
+        }
+      }
+
+      global = {
+        credentials = {
+          apiKey = var.datadog_api_key
+          appKey = var.datadog_app_key
+        }
+
+        site = "datadoghq.com"
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.datadog
   ]
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "datadog_app_key" {
+  description = "The Datadog API key"
+  type        = string
+}
 variable "datadog_api_key" {
   description = "The Datadog API key"
   type        = string
